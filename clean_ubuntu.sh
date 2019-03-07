457b14b6f09e65d09bdac000d6a083cab56382d3
Vighnesh Maheshwari <21048293+vigh-m@users.noreply.github.com>
2019-03-07 15:19:30 -0500

    Script to clean Ubuntu 16.04 after install
    Just uninstalls some unneeded software and updates.

---
+++ clean_ubuntu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt-get -y remove libreoffice-*
+sudo apt-get -y remove thunderbird
+sudo apt-get -y remove firefox
+sudo apt-get -y remove rhythmbox
+sudo apt-get -y remove rhythmbox-data 
+sudo apt-get -y remove cheese
+sudo apt-get -y remove cheese-common 
+sudo apt-get -y remove aisleriot
+sudo apt-get -y remove gnome-mahjongg
+sudo apt-get -y remove transmission-*
+sudo apt-get -y remove gnome-mines
+sudo apt-get -y remove gnome-calculator
+sudo apt-get -y remove gnome-calendar
+sudo apt-get -y remove shotwell
+sudo apt-get -y remove shotwell-common
+sudo apt-get -y remove remmina*
+sudo apt-get -y remove totem*
+sudo apt-get -y remove evince
+sudo apt-get -y autoclean 
+sudo apt-get -y autoremove 
+sudo apt-get -y update
+sudo apt-get -y upgrade 
+sudo apt-get -y autoclean 
